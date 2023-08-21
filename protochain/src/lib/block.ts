34a5af8f5c361c8e78ee729be8bd694fb93edc0a
Ismael Spaniol <ismaelspaniol@outlook.com>
2023-08-21 20:02:56 -0300

    validacao e interligamento blockchain

--- protochain/src/lib/block.ts
+++ protochain/src/lib/block.ts
@@ -1,27 +1,47 @@
-
+import sha256 from 'crypto-js/sha256';
 /**
  * Block class
  */
 export default class Block{
     index: number;
+    timestamp : number;
     hash: string;
+    previousHash: string;
+    data: string;
 
+
+    
     /**
      * Create a new block
      * @param index The block index in blockchanin
-     * @param hash The block hash
+     
+     * @param previousHash The previous block hash
+     * @param data The block data
      */
-    constructor(index: number, hash: string){
-        this.hash = hash;
+    constructor(index: number,  previousHash: string, data: string){
+        
         this.index = index;
+        this.timestamp = Date.now();
+        this.previousHash = previousHash;
+        this.data = data;
+
+        this.hash = this.getHash();
+    }
+
+    getHash():string{
+        return sha256(this.index + this.data + this.timestamp + this.previousHash).toString();
     }
     /**
      *  Validates the block
      * @returns Return is validate block
      */
-    isValid():boolean{
-        if(this.index < 0) return false;
-        if(!this.hash) return false;
+    isValid(previousHash : string, previousIndex : number):boolean{
+        if(previousIndex !== this.index - 1) return false;
+        if(this.hash !== this.getHash()) return false;
+        if(!this.data) return false;
+        if(this.timestamp < 1) return false;
+        if(this.previousHash !== previousHash) return false;
+
         return true;
 
     }
