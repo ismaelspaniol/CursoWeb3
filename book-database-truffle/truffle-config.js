563f3581ce91aabb463bbf13d16b39cc4fadafe6
Ismael Spaniol <ismaelspaniol@outlook.com>
2023-09-26 20:31:05 -0300

    truffle deploy

--- book-database-truffle/truffle-config.js
+++ book-database-truffle/truffle-config.js
@@ -1,8 +1,30 @@
+require("dotenv").config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
-  networks: {},
+  plugins: ["truffle-plugin-verify"],
+  api_keys: {
+    etherscan: process.env.API_KEY
+  },
+  networks: {
+    goerli: {
+      provider: new HDWalletProvider({
+        mnemonic: {
+          phrase: process.env.SECRET
+        },
+        providerOrUrl: process.env.INFURA_URL
+      }),
+      network_id: "11155111"
+    },
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777"
+    }
+  },
   compilers: {
     solc: {
-      version:"0.8.17",
+      version: "0.8.17",
       settings: {
         optimizer: {
           enabled: true, // Default: false
